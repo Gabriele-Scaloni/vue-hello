301e8228514f5c41ad89a5ed1375095819c5f73c
Gabriele-Scaloni <88436304+Gabriele-Scaloni@users.noreply.github.com>
2021-11-10 16:57:20 +0100

    finito

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 /* Stampare a schermo un messaggio all’interno di un h1,
 utilizzando i data. */
 
-var messaggio = new Vue({
-    el: '#messaggio',
+var myApp = new Vue({
+    el: '#myApp',
     data: {
       message: 'Benvenuto'
+    }
+})
+
+var app = new Vue({
+    el: '#app',
+    data: {
+     nome : '',
+     cognome:''
+     
     }
 })
